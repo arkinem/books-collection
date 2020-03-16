e61029106fb36bca60fd809494d5a357878b5d5d
Blazej Golinski <blazej.golinski@virtualclinic.direct>
2020-03-16 10:58:52 +0000

    adds react bootstrap with custom theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
+import "./custom.scss";
 import AppRouter from "./navigation/AppRouter";
 import { Reset } from "styled-reset";
 
--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 const BooksPage = () => {
-  return <div>Books</div>;
+  return (
+    <div>
+      Books
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
 };
 
 export default BooksPage;
