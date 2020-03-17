d11e940c02b86eba8dbb1bff9b66185283411728
Blazej Golinski <blazej.golinski@virtualclinic.direct>
2020-03-17 14:16:52 +0000

    removes tests directory

--- src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import BooksPage from "../pages/BooksPage";
-
-test("renders learn react link", () => {
-  const { getByText } = render(<BooksPage />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
